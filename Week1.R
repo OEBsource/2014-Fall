61828610b482159c78bafa0cae27c976ce28cabf
Michael Rosario <michaelvrosario@gmail.com>
2014-09-19 13:02:24 -0400

    Update Week1.R

--- Week1.R
+++ Week1.R
@@ -1,6 +1,8 @@
 #Two ways to set a working directory.
 setwd("/Users/Home/Dropbox/Amherst/Courses/SOURCE/2014-Fall")
 
+#What's the second way?
+
 HW<-read.table("HandW.txt", sep="\t")
 HW<-read.table("HandW.txt", sep=" ")
 
