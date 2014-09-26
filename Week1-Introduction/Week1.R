82de565b3e2e1ad9c3b837b2a962a4e26c6fb62e
AndrewJohnSmith <ajsmith@bio.umass.edu>
2014-09-26 11:29:02 -0400

    Addressing a question (from Mike?)

--- Week1-Introduction/Week1.R
+++ Week1-Introduction/Week1.R
@@ -2,6 +2,7 @@
 setwd("/Users/Home/Dropbox/Amherst/Courses/SOURCE/2014-Fall")
 
 #What's the second way?
+#Go to 'Session' > 'Set Working Directory' > 'Choose Directory' in the top toolbar.
 
 HW<-read.table("HandW.txt", sep="\t")
 HW<-read.table("HandW.txt", sep=" ")
